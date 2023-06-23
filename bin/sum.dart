3fbd04a8beaa41c3abb7a34ef29abc06c512793f
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-23 17:59:09 +0530

    Initial Commit

--- bin/sum.dart
+++ bin/sum.dart
@@ -1,16 +1,13 @@
 import 'dart:io';
 
-void main()
-{
-  int sum=0;
+void main() {
+  int sum = 0;
   print("Sum of Odd Numbers from 1-10: ");
-  for (int i = 1; i<=10 ; i++){
-    if(i % 2 != 0){
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 != 0) {
       stdout.write("$i ");
     }
-    sum= sum+i;
+    sum = sum + i;
   }
   print("\nSum =$sum");
-
 }
-
