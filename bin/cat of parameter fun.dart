fa42f0779f1a654724b1471ec50071b045bf54ef
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-30 14:15:20 +0530

    Initial Commit

--- bin/cat of parameter fun.dart
+++ bin/cat of parameter fun.dart
@@ -15,6 +15,26 @@ void func2(String name, {int? age, int? phone, String? email}) {
   print('Email = $email');
 }
 
+void func3(String name,
+    {int? age, required int? phone, required String? email}) {
+  print('name = $name');
+  print('age = $age');
+  print('phone = $phone');
+  print('Email = $email');
+}
+
+void func4(String name,
+    {int? age, required int? phone, required String email, int year = 2023}) {
+  print('name = $name');
+  print('age = $age');
+  print('phone = $phone');
+  print('Email = $email');
+  print('Year = $year');
+
+}
+
+///3. Optional Named parameter with required arguments
+
 void main() {
   func1("Hanna", 11);
 
@@ -23,4 +43,6 @@ void main() {
 // cannot skip any arguments
 
   func2("Anitha", phone: 9835267829);
+  func3("Arun", phone: 9056789056, email: "arun@gmail.com");
+  func4("TIn", phone: 90567845656, email: "tin.com");
 }
