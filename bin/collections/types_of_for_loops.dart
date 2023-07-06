89bd8841440219d80b1eef81879b4e77a23db303
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-06 13:55:28 +0530

    Initial Commit

--- bin/collections/types_of_for_loops.dart
+++ bin/collections/types_of_for_loops.dart
@@ -1,10 +1,15 @@
 void main() {
   List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  //1. Normal For Loop
   for (int index = 0; index < list.length; index++) {
     print(list[index]);
   }
-  for(int i in list)
-    {
 
-    }
+  print('\n \n');
+
+  //2. For - in Loop syntax for(datatype-similar-to-list variable name in listname){}
+  for (int i in list) {
+    print(i);
+  }
 }
