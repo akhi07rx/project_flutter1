2281836ccf181a69322c6e435f1ff253b81defc0
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-12 15:00:55 +0530

    Initial Commit

--- bin/oops/static_keyword.dart
+++ bin/oops/static_keyword.dart
@@ -1,18 +1,24 @@
 class A {
   String? name;
-  ststic String company ="Luminar";
+  static String company = "Luminar";
   final int year = 2023;
 
-  void show()
-  {
-    print("My name is ${name = "Anju"}, I am working at $company from $year");
+  static void show() {
+    print("I am working at $company from $year");
   }
-
+//Static method can access static and local variable only, it cant access instance variables
 }
 
-void main()
-{
+
+void main() {
   A.obj = A();
-  obj.show();
+  print("My name is ${obj.name = "Anju"});
+      A.show()
+  print(I am working at ${A.company} from ${obj.year}");
 
-}
+
+      //static variable and functions depends on class
+      // so call them like :- ClassName.static_variable_name;
+      //                   :- ClassName.method_name;
+
+  }
